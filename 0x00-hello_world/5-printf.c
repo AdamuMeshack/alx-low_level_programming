5fdf83a6a0e1a0f7aff7d139ad0f387eab6763ca
AdamuMeshack <meshackatama@gmail.com>
2022-09-08 17:00:31 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - printing a statement in the function
+ *
+ * description usining the function printf
+ * it prints the script using printf
+ * Return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
