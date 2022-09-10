793bf060fe5e46830ae05332a9d3c2f22232819d
AdamuMeshack <meshackatama@gmail.com>
2022-09-10 09:13:41 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,14 +4,23 @@
 /**
  * main - Entry point
  *
+ * Description: using the main function
+ *
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++)
+	for (low = 'a' ; low <= 'z' ; low++)
+	{
 		putchar(low);
-		putchar("\n");
+		
+	}
+	for (low = 'A' ; low <= 'Z' ; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
 	return (0);
 }
