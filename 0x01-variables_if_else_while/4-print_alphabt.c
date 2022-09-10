5753759d9b32166edbafe18a3fd854b483d14685
AdamuMeshack <meshackatama@gmail.com>
2022-09-10 09:37:41 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - print the alphebet in lower followed by newline
+ *
+ * Description: using the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char low;
+	for (low = 'a' ; low <= 'z' ; low++)
+	{
+	if (low != 'e' && low != 'q')
+	{
+		putcher(low);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
