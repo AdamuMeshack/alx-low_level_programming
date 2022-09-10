068db756dafe1667fda568d0b25893df38599c27
AdamuMeshack <meshackatama@gmail.com>
2022-09-10 12:05:54 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints all single digit numbers
+ *
+ * Description: a program that prints all single digit numbers of base
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (i = '0' ; i <= '9' ; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n';)
+	return (0);
+}
