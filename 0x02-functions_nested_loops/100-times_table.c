f86945c2e33a2325d54822fff5523074a8edb232
AdamuMeshack <meshackatama@gmail.com>
2022-09-14 02:57:28 -0700

    question 12 and 13

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,3 +6,40 @@
  * Return: nothing
  */
 void three_dig(int n)
+{
+	int i, j, product;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				product = i * j;
+				if (product > 99)
+				{
+					three_dig(product);
+				}
+				else if (product / 10 > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product / 10 + '0');
+					_putchar((product) % 10 + '0');
+				}
+				else if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else
+					_putchar(product % 10 + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
