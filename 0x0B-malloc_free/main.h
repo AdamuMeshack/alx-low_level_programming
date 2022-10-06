a326c9ce3a3ccb873b9cd6e25975a869a40c28c0
AdamuMeshack <meshackatama@gmail.com>
2022-10-06 13:37:24 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -8,5 +10,6 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
